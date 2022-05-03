641582af40cd41969ba6862931050949d76d46c5
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-05-03 15:19:33 -0400

    buttons and pixel grid are functional

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const tiles = document.querySelector('.tiles');
-let selector = document.querySelector('#selector');
-const clear = document.querySelector('clear');
+let selector = document.querySelectorAll('input[name="mySelect"]');
+const clear = document.getElementById('clear');
+let val;
 
 function createTiles(size) {
     tiles.style.setProperty('--size', size);
     for (i = 0; i < size * size; i++) {
         const div = document.createElement('div');
         div.classList.add('aTile');
+        div.addEventListener('mouseover', function () {
+            div.style.backgroundColor = '#f70030';
+        })
         tiles.appendChild(div);
     }
 }
-createTiles(16)
+
+function reset() {
+    tiles.innerHTML = '';
+    createTiles(val);
+}
+clear.addEventListener('click', reset)
+
+
+for (let i = 0; i < selector.length; i++) {
+    selector[i].addEventListener("change", function () {
+        val = this.value;
+        reset();
+    })
+}
+createTiles(val);
